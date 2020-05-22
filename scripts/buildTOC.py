c1078f2e857ffc99a0b220d900735e572e1d3188
Anders Lind <mail@anderslind.se>
2020-05-22 20:03:49 +0200

    Added environment.yml and started adding CLI to readme generator

--- scripts/buildTOC.py
+++ scripts/buildTOC.py
@@ -5,16 +5,23 @@ A script to generate a Table Of Content from the folders
 and any markdown files within these folders.
 """
 
-#Import libraries
+# Import libraries
 import os
+import click
 
-#Set the path
-basepath='/Users/anli/code/TIL'
+# Get arguments from the commandline
+@click.command()
+@click.option('--tempalte', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
 
-#folders to exclude
+# Set the path
+basepath = '/Users/anli/code/TIL'
+
+# folders to exclude
 skipfold = {"scripts"}
 
-#Find all relevant folders
+# Find all relevant folders
 folders = []
 for entry in os.scandir(basepath):
     if entry.is_dir():
@@ -24,11 +31,36 @@ for entry in os.scandir(basepath):
             continue
         folders.append(entry.name)
 
-print("Folders to include")
-for f in folders:
-    print(f)
+for folder in sorted(folders):
+    print(folder)
+print("---")
+# Find all markdown in all folders
+foldstruct = {}
 
-#Find all 
+for folder in sorted(folders):
+    print(folder)
+    for file in os.scandir(folder):
+        if file.is_file() and file.name.endswith('.md'):
+            foldstruct[folder] = file
+            print(file.name)
+
+for folder, file in foldstruct.items():
+    with open(file.path, "r") as md:
+        heads = []
+        for line in md:
+            if line.startswith("### "):
+                print(line)
+            if line.startswith("##### "):
+                print(line)
+
+# Open a file and read stuff
+# with open("/Users/anli/code/TIL/conda/condacheat.md","r") as file:
+#     id = []
+#     for ln in file:
+#         if ln.startswith("####"):
+#             nohead = ln[5:]
+#             id.append(nohead[:-1])
+# print(id)
 
 """
 if __name__ == '__main__':
